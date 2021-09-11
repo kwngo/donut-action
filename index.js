294faa318001776c9994bcfc7d044e02958d992f
Kareem Kwong <kareem.kwong@gmail.com>
2021-09-11 19:45:03 -0400

    Break out day checking logic. Add basic tests

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const core = require('@actions/core');
-const wait = require('./wait');
 var dayjs = require('dayjs')
 var utc = require('dayjs/plugin/utc')
 var timezone = require('dayjs/plugin/timezone') // dependent on utc plugin
 var isToday = require('dayjs/plugin/isToday')
+
+var today = require('./today')
 
 
 dayjs.extend(utc)
@@ -13,28 +14,20 @@ dayjs.extend(isToday)
 dayjs.tz.setDefault("America/New_York")
 
 
-var daysToIndex = {
-  'sunday': 0, 
-  'monday': 1,
-  'tuesday': 2,
-  'wednesday': 3,
-  'thursday': 4,
-  'friday': 5,
-  'saturday': 6,
-}
+
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     var days = core.getInput('days', {required: true})
     var timezoneName = core.getInput('timezone')
     var listOfDays = days.split(",")
+    var now = dayjs()
 
     core.setOutput('deploy', true)
     dayjs.tz.setDefault(timezoneName)
     core.info("Set timezone to " + timezoneName)
 
-    const isNotToday = (day) => now.day != daysToIndex[day];
-    var deploy = listOfDays.every(isNotToday)
+    var deploy = listOfDays.every(today(now))
     if (!deploy) {
       core.setOutput('deploy', false)
     }
--- index.test.js
+++ index.test.js
@@ -1,3 +1,26 @@
-test('placeholder test', async () => {
-  expect(false).toBeFalsy()
+var isNotToday = require('./isNotToday')
+var dayjs = require('dayjs')
+var utc = require('dayjs/plugin/utc')
+var timezone = require('dayjs/plugin/timezone') // dependent on utc plugin
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.tz.setDefault("America/New_York")
+
+test('test check if saturday false', async () => {
+    var now = dayjs().day(6)
+    var check = isNotToday(now)
+    expect(check('saturday')).toEqual(false)
+});
+
+test('test check if sunday false', async () => {
+    var now = dayjs().day(0)
+    var check = isNotToday(now)
+    expect(check('sunday')).toEqual(false)
+});
+
+test('test check if sunday true', async () => {
+    var now = dayjs().day(0)
+    var check = isNotToday(now)
+    expect(check('monday')).toEqual(true)
 });
